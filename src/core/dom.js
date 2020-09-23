46f3d946390024626a3c5708379f84865ca660a2
NikolaiKryshnev <63440682+NikolaiKryshnev@users.noreply.github.com>
2020-09-23 12:26:07 +0300

    Finish base table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,78 +1,118 @@
 class Dom {
-  constructor(selector) {
-    this.$el = typeof selector === 'string'
-      ? document.querySelector(selector)
-      : selector
-  }
+	constructor(selector) {
+		this.$el = typeof selector === 'string'
+			? document.querySelector(selector)
+			: selector
+	}
 
-  html(html) {
-    if (typeof html === 'string') {
-      this.$el.innerHTML = html
-      return this
-    }
-    return this.$el.outerHTML.trim()
-  }
+	html(html) {
+		if (typeof html === 'string') {
+			this.$el.innerHTML = html
+			return this
+		}
+		return this.$el.outerHTML.trim()
+	}
 
-  clear() {
-    this.html('')
-    return this
-  }
+	text(text) {
+		if (typeof text === 'string') {
+			this.$el.textContent = text
+			return this
+		}
+		if (this.$el.tagName.toLowerCase() === 'input') {
+			return this.$el.value.trim()
+		}
+		return this.$el.textContent.trim()
+	}
 
-  on(eventType, callback) {
-    this.$el.addEventListener(eventType, callback)
-  }
+	clear() {
+		this.html('')
+		return this
+	}
 
-  off(eventType, callback) {
-    this.$el.removeEventListener(eventType, callback)
-  }
+	on(eventType, callback) {
+		this.$el.addEventListener(eventType, callback)
+	}
 
-  append(node) {
-    if (node instanceof Dom) {
-      node = node.$el
-    }
+	off(eventType, callback) {
+		this.$el.removeEventListener(eventType, callback)
+	}
 
-    if (Element.prototype.append) {
-      this.$el.append(node)
-    } else {
-      this.$el.appendChild(node)
-    }
+	find(selector) {
+		return $(this.$el.querySelector(selector))
+	}
 
-    return this
-  }
+	append(node) {
+		if (node instanceof Dom) {
+			node = node.$el
+		}
 
-  get data() {
-    return this.$el.dataset
-  }
+		if (Element.prototype.append) {
+			this.$el.append(node)
+		} else {
+			this.$el.appendChild(node)
+		}
 
-  closest(selector) {
-    return $(this.$el.closest(selector))
-  }
+		return this
+	}
 
-  getCoords() {
-    return this.$el.getBoundingClientRect()
-  }
+	get data() {
+		return this.$el.dataset
+	}
 
-  findAll(selector) {
-    return this.$el.querySelectorAll(selector)
-  }
+	closest(selector) {
+		return $(this.$el.closest(selector))
+	}
 
-  css(styles = {}) {
-    Object
-        .keys(styles)
-        .forEach(key => {
-          this.$el.style[key] = styles[key]
-        })
-  }
+	getCoords() {
+		return this.$el.getBoundingClientRect()
+	}
+
+	findAll(selector) {
+		return this.$el.querySelectorAll(selector)
+	}
+
+	css(styles = {}) {
+		Object
+			.keys(styles)
+			.forEach(key => {
+				this.$el.style[key] = styles[key]
+			})
+	}
+
+	id(parse) {
+		if (parse) {
+			const parsed = this.id().split(':')
+			return {
+				row: +parsed[0],
+				col: +parsed[1]
+			}
+		}
+		return this.data.id
+	}
+	focus() {
+		this.$el.focus()
+		return this
+	}
+	addClass(className) {
+		this.$el.classList.add(className)
+		return this
+	}
+
+	removeClass(className) {
+		this.$el.classList.remove(className)
+		return this
+
+	}
 }
 
 export function $(selector) {
-  return new Dom(selector)
+	return new Dom(selector)
 }
 
 $.create = (tagName, classes = '') => {
-  const el = document.createElement(tagName)
-  if (classes) {
-    el.classList.add(classes)
-  }
-  return $(el)
+	const el = document.createElement(tagName)
+	if (classes) {
+		el.classList.add(classes)
+	}
+	return $(el)
 }
